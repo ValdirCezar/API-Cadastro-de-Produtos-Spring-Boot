a7801abb11ddcb81e28983693c24fbcd57925e23
= <=>
2020-11-10 11:27:17 -0300

    Classe de domínio Produto criada

---
+++ src/main/java/com/valdir/apistore/domain/Produto.java
@@ -0,0 +1,73 @@
+package com.valdir.apistore.domain;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+	private Double preco;
+
+	public Produto() {
+		super();
+	}
+
+	public Produto(Integer id, String nome, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
