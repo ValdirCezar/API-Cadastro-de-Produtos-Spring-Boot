6152085cac0920ddc4ed41204e139d35b896ca91
= <=>
2020-11-10 11:03:28 -0300

    commit inicial

---
+++ src/main/java/com/valdir/apistore/ApistoreApplication.java
@@ -0,0 +1,13 @@
+package com.valdir.apistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApistoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApistoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valdir/apistore/ApistoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdir.apistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApistoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
