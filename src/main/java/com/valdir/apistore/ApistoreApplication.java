33bf44b1e5a57d8d169d4af6cedace67daab37f4
= <=>
2020-11-10 11:24:12 -0300

    Criação automatica da base de dados com H2 Database

--- src/main/java/com/valdir/apistore/ApistoreApplication.java
+++ src/main/java/com/valdir/apistore/ApistoreApplication.java
@@ -1,13 +1,34 @@
 package com.valdir.apistore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.valdir.apistore.domain.Categoria;
+import com.valdir.apistore.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ApistoreApplication {
+public class ApistoreApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApistoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Ferramentas");
+		Categoria cat3 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 	}
 
 }
